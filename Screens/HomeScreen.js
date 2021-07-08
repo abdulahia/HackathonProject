5a873382be967fc2f115a2be204ca05bb3df43be
abdulahia <abdulahia@berea.edu>
2021-07-07 23:31:15 -0400

    Implemented bottom navigation tab and the four screens

---
+++ Screens/HomeScreen.js
@@ -0,0 +1,57 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { LinearGradient } from "expo-linear-gradient";
+import MaterialIcons from "react-native-vector-icons";
+import * as Animatable from "react-native-animatable";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  SafeAreaView,
+  Dimensions,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.view}>
+        <Text style={styles.text}>Home Screen</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#61EEFE",
+  },
+  header: {
+    flex: 2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  footer: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    borderTopRightRadius: 30,
+    borderTopLeftRadius: 30,
+    paddingVertical: 50,
+    paddingHorizontal: 30,
+    bottom: "0%",
+  },
+  text: {
+    textAlignVertical: "center",
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  view: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
